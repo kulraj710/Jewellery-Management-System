aa896134e88209e76d935ab41f379b1b09e6cf42
kulraj chavda <kulraj710@gmail.com>
2024-06-18 23:53:41 +0530

    [feat] : simple navbar with links

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductPage from './Components/Product/ProductPage';
 import HomePage from './Components/Home/HomePage';
+import Navbar from './Layouts/Navbar';
 
 function App() {
   return (
     <>
   <Router>
+  <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product" element={<ProductPage />} />
--- Frontend/src/Layouts/Navbar.jsx
+++ Frontend/src/Layouts/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "../Styles/Layouts/Navbar.css"
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      TODO : Implement Navbar design
+    <nav className="navbar">
+      <div className="container">
+        <h1 className="logo">Store Name</h1>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/product" className="nav-link">Inventory</Link>
+          </li>
+          {/* Add more links as needed */}
+        </ul>
+      </div>
     </nav>
   )
 }
