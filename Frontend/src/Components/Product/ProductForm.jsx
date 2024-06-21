b891d8787670466b6109d37ba634ab38c37179f0
kulraj chavda <kulraj710@gmail.com>
2024-06-21 23:42:32 +0530

    [feat] : setup card detail/form navigation

--- Frontend/src/Components/Product/ProductForm.jsx
+++ Frontend/src/Components/Product/ProductForm.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import ButtonComponent from '../../Layouts/ButtonComponent';
 
 const ProductForm = () => {
     const { id } = useParams();
-  return (
-    <div>
-      this is product with id : {id}
-    </div>
-  )
+    const navigate = useNavigate();
+
+    const RedirectBackToProducts = () => {
+        navigate('/product')
+    }
+
+    const containerStyles = {
+        
+    }
+    const formStyles = {
+        backgroundColor : '#f5f5f5',
+        margin : '2rem'
+    }
+    return (
+        <section style={containerStyles}>
+            <div>
+                <ButtonComponent title="Back" onClick={RedirectBackToProducts} />
+            </div>
+
+            <div style={formStyles}>
+                this is product with id : {id}
+            </div>
+        </section>
+    )
 }
 
 export default ProductForm
--- Frontend/src/Layouts/ButtonComponent.jsx
+++ Frontend/src/Layouts/ButtonComponent.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 
-const ButtonComponent = (title, padding = '1rem') => {
+const ButtonComponent = ({title, 
+  padding = '10px',
+  width = 'default',
+  backgroundColor = '#007bff',
+  color = 'white',
+  border = 'none', 
+  borderRadius = '4px',
+  onClick,
+  cursor = 'pointer'}) => {
 
   // TO-DO : Implement primary and secondary button styling
+
   const styles = {
-    padding : padding
+    padding : padding,
+    width: width,
+    backgroundColor: backgroundColor,
+    color: color,
+    border: border,
+    borderRadius: borderRadius,
+    cursor: cursor
   }
 
   return (
-    <button style={styles}>
+    <button style={styles} onClick={onClick}>
         {title}
     </button>
 
