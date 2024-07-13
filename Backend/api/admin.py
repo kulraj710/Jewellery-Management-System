80494e935face7e990314833cd6c59c87ce24769
kulraj chavda <kulraj710@gmail.com>
2024-07-14 04:10:02 +0530

    [chore] : register admin models

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, ProductCategory, ProductUsers, ProductSupplier, ProductMaterial
 
 admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductUsers)
+admin.site.register(ProductSupplier)
+admin.site.register(ProductMaterial)
