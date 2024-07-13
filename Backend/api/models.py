244fe06e3c12982ca2e9a4d7d9c59060d13df7a7
kulraj chavda <kulraj710@gmail.com>
2024-07-14 04:11:12 +0530

    [refactor] (db) : addnormalization on Product table, add 4 new tables

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
 
+
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=255)
+    
+class ProductUsers(models.Model):
+    username = models.CharField(max_length=255)
+
+class ProductSupplier(models.Model):
+    partner_name = models.CharField(max_length=255)
+    
+class ProductMaterial(models.Model):
+    material_type = models.CharField(max_length=20)
+    
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
     product_code = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True)
     brand = models.CharField(max_length=255, null=True, blank=True)
-    material_type = models.CharField(max_length=255, null=True, blank=True)
+    material_type = models.ForeignKey(ProductMaterial, on_delete=models.DO_NOTHING)
     purity = models.CharField(max_length=255, null=True, blank=True)
     grweight = models.CharField(max_length=255)
     ntweight = models.CharField(max_length=255)
@@ -17,19 +30,17 @@ class Product(models.Model):
     customizable = models.BooleanField(default=False)
     cost_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    discount = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    discount = models.CharField(max_length=5, default="0")
     stock_quantity = models.IntegerField()
-    supplier = models.CharField(max_length=255, null=True, blank=True)
-    product_image = models.ImageField(upload_to='product_images/', null=True)
-    product_video = models.URLField(blank=True, null=True)
+    supplier = models.ForeignKey(ProductSupplier,on_delete=models.DO_NOTHING, null=True, blank=True)
+    product_image = models.ImageField(upload_to='product_images/', null=True, blank=True)
     certification_details = models.TextField(blank=True, null=True)
     warranty = models.BooleanField(default=False)
     care_instructions = models.TextField(blank=True, null=True)
     product_tags = models.CharField(max_length=255, blank=True, null=True)
-    date_of_entry = models.DateField()
-    entered_by = models.CharField(max_length=255)
+    date_of_entry = models.DateTimeField()
+    entered_by = models.ForeignKey(ProductUsers, on_delete=models.DO_NOTHING)
     product_status = models.CharField(max_length=50, default='Active')
 
     def __str__(self):
         return self.product_name
-
--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, ProductCategory, ProductMaterial, ProductSupplier, ProductUsers
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+class ProductUsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductUsers
+        fields = '__all__'
+
+class ProductSupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductSupplier
+        fields = '__all__'
+
+class ProductMaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductMaterial
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = ProductCategorySerializer()
+    material_type = ProductMaterialSerializer()
+    supplier = ProductSupplierSerializer(allow_null=True)
+    entered_by = ProductUsersSerializer()
+
     class Meta:
         model = Product
         fields = '__all__'
