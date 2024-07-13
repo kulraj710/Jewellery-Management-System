e1262e47f39d3288a5ad5a4637b01ba73664fbe9
kulraj chavda <kulraj710@gmail.com>
2024-07-14 00:17:20 +0530

    [style] : update style of card

--- Frontend/src/Components/Product/ProductCard.jsx
+++ Frontend/src/Components/Product/ProductCard.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import "../../Styles/Product/ProductCard.css"
 import { Link } from "react-router-dom"
+import placeholderImage from "../../assets/placeholder.png"
+import Badge from '../../Layouts/Badge'
 
 // Currently, hard coded data,
 // TO-DO : Fetch Data from API
 
-const ProductCard = ({product}) => {
- 
+const ProductCard = ({ product }) => {
+
   return (
-    <div>
-      <Link to={"/product/" + product.id} style={{ textDecoration : 'none'}}>
-      <div className="product">
-        <img src="https://i.ibb.co/pQfxwdL/ring.jpg" alt="Areca Jewelry" />
-        <div className="product-info">
-          <p><strong>Product Name:</strong> {product.product_name}</p>
-          <p><strong>HSN Code:</strong> {product.product_code}</p>
-          <p><strong>Gross Weight:</strong>{product.weight} g</p>
-          <p><strong>Net Weight:</strong> {product.weight}</p>
-          <p><strong>Quantity on Hand:</strong> {product.stock_quantity}</p>
-          <p><strong>Category:</strong> {product.category}</p>
+    <div className='product-card-container'>
+      <Link to={"/product/" + product.id} style={{ textDecoration: 'none' }}>
+        <div className="product" style={{ height: '300px', display: 'flex', flexDirection: 'column' }}>
+
+          {product.product_image ? <img src={`http://localhost:8000/api${product.product_image}`} width={'200px'} height={'200px'} alt={product.product_name} /> :
+            <img src={placeholderImage} width={'200px'} height={'200px'} alt={"No Image"} />
+          }
+
+          <div className="product-info">
+            <p id='product-name-text'> {product.product_name}</p>
+            <p id='product-hsn-text'>HSN :  <strong>{product.product_code}</strong></p>
+            <p><strong>Qty:</strong> {product.stock_quantity}</p>
+
+            <Badge addClass='product-gr-text' title={`Gr Wt : ${product.grweight} g`} bgcolor='lightgrey' color='black' border='2px solid lightgrey'/>
+            <Badge addClass='product-nt-text' title={`Nt Wt : ${product.ntweight} g`} bgcolor='lightgrey' color='black' border='2px solid lightgrey'/>
+            
+            <p id='product-category-text'><strong>{product.category}</strong></p>
+          </div>
         </div>
-      </div>
       </Link>
     </div>
   )
